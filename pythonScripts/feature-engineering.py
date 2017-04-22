b6b00b078d6c7f6adb3b77310295d14b441d4210
akshaytrivedi <trivedi.akshay3@gmail.com>
2017-04-22 19:38:36 -0400

    Removing dependecy on graphlab

--- pythonScripts/feature-engineering.py
+++ pythonScripts/feature-engineering.py
@@ -1,12 +1,12 @@
-
 from sklearn.neighbors import NearestNeighbors
 # we need to tune the number of neighbours
 neigh = NearestNeighbors(n_neighbors=11)
 neigh.fit(tf_idf)
 
 
-sc_lc = pd.read_csv('../data/sc_lc.csv')
+sc_lc = pd.read_csv('C:\Users\Admin\Documents\ml\project\scotus\sc_lc.csv')
 
+data = pd.read_csv('C:\Users\Admin\Documents\ml\project\scotus\data\complete_data.csv')
 
 # # Case similarity approach
 # ### Algorithms
@@ -26,43 +26,14 @@ sc_lc = pd.read_csv('../data/sc_lc.csv')
 # 3. Take majority vote of 10 nearest neighbour, and take that as predicted output.
 # 4. accuracy = number of correct predictoins / total number of cases
 
-
 def get_case_outcomes_for_file(file):
     # NOTE: -2 is used since the files were in .p format 2 is length of ".p"
     # If extension changes we need to change this .2
     if len(sc_lc[sc_lc['caseid']==file[:-2]]['case_outcome_disposition'].values) > 0:
         return sc_lc[sc_lc['caseid']==file[:-2]]['case_outcome_disposition'].values[0]
     else:
-        print "File not found "+file
+        print "File not found "+ file
         return -1
-
-
-def get_compare_case_outcomes(cases):
-    '''
-    TODO: remove the actual case whose neighbour are being considered
-    '''
-    neighbour_filename_outcome = []
-    affirm = 0
-    reverse = 0
-    outcome = 0
-    for idx,case in enumerate(cases):
-        case_outcome = get_case_outcomes_for_file(case)
-        if case_outcome == -1:
-            continue
-        neighbour_filename_outcome.append({'file': case, 'outcome': case_outcome})
-        if case_outcome == 1:
-            affirm = affirm + 1
-        else:
-            reverse = reverse + 1
-
-    if affirm>reverse:
-        outcome = 1
-    else:
-        # if number of affirms is same as reverse we predict reverse.
-        # this assumption is made making use of scotus-1, where baseline classifier always predicts reverse.
-        outcome = 0
-    return neighbour_filename_outcome,outcome
-
 
 
 def get_filtered_tf_idf(num_correct=5, num_incorrect=5):
@@ -78,12 +49,12 @@ def get_filtered_tf_idf(num_correct=5, num_incorrect=5):
 
         file_distance_dict = {}
         for index,n in enumerate(neighs):
-            key = str(data[data['id']==n]['filename'][0])
+            key = data[data['id']==n]['filename'].iloc[0]
             file_distance_dict[key] = distances[index]
 
-        similar_cases = data.filter_by(neighs, 'id')['filename']
+        similar_cases = data[data['id'].isin(neighs)]['filename'].tolist()
         df,predicted_outcome = get_compare_case_outcomes(similar_cases)
-        query_file = data[data['id']==idx]['filename'] [0]
+        query_file = data[data['id']==idx]['filename'].iloc[0]
         actual_outcome = get_case_outcomes_for_file(query_file)
 
         if actual_outcome == predicted_outcome:
@@ -91,7 +62,6 @@ def get_filtered_tf_idf(num_correct=5, num_incorrect=5):
         else:
             incorrect = incorrect + 1
 
-        print str(idx) + " correct: " + str(correct) + "incorrect: " + str(incorrect)
         nearest_neighbour_data[idx] = {'query_file': query_file,
                                        'similar_cases': similar_cases,
                                        'distances' : file_distance_dict,
@@ -108,12 +78,7 @@ def get_filtered_tf_idf(num_correct=5, num_incorrect=5):
 
 overall_correct, overall_incorrect, nearest_neighbour_data = get_filtered_tf_idf()
 accuracy = float(overall_correct)/float(overall_incorrect+overall_correct)
-
-
 print accuracy*100
-
-
-
 
 
 
