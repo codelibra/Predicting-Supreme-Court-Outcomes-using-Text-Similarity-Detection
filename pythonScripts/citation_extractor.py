4b21ae38bc256ec400f5b90c94a069355f3834f9
Shivendra Panicker <shivendranitb@gmail.com>
2017-04-25 16:44:04 -0400

    Extracted citations from all circuit courses in the corpus

--- pythonScripts/citation_extractor.py
+++ pythonScripts/citation_extractor.py
@@ -1,11 +1,32 @@
 import re
 import pickle
+import pandas as pd
+import os
 
 def get_citations_from_file(filename):
     res = pickle.load(open( filename, "rb" ))
     res = " ".join(res)
-    district_court_pattern = re.findall("\d+ F\. Supp\.( \dd)? \d+",res)
-    circuit_court_pattern = re.findall("\d+ F\.(\dd)? \d+",res)
-    supreme_court_pattern = re.findall("\d+ U\.S\. \d+",res)
-    usc_pattern = re.findall("\d+ U\.S\.C\. .. \d+",res)
-    print district_court_pattern,circuit_court_pattern,supreme_court_pattern,usc_pattern
+    all_citations = set()
+    all_citations |= set(re.findall("\d+ F\. Supp\.(?: \dd) \d+",res))
+    all_citations |= set(re.findall("\d+ F\.(?:\dd) \d+",res))
+    all_citations |= set(re.findall("\d+ U\.S\. \d+",res))
+    all_citations |= set(re.findall("\d+ U\.S\.C\. .. \d+",res))
+    return all_citations
+
+get_citations_from_file('../data/circuit-scbd-mapped-files/X125RLM003.p')
+
+
+count = 0
+citations_dict  = {}
+for root, dirs, files in os.walk('../data/circuit-scbd-mapped-files/', topdown=False):
+    for idx,name in enumerate(files):
+        if ".p" in name:
+            citations_dict[name] = get_citations_from_file(root+name)
+        print count
+        count = count + 1
+
+
+citations = pd.DataFrame(citations_dict)
+
+
+citations_dict['X125RLM003.p']
