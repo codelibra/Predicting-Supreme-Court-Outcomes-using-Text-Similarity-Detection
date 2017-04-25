6243b3d684a908f551df28bfc6b212627fd8455f
Shivendra Panicker <shivendranitb@gmail.com>
2017-04-25 14:36:03 -0400

    searched for circuit court files -5 years from actual supreme court term. Improved search misses. Only 14 files not found

--- pythonScripts/text-mapping-circuit-filesystem.py
+++ pythonScripts/text-mapping-circuit-filesystem.py
@@ -7,20 +7,37 @@
 import pandas as pd
 import pickle
 import sys
-
+import os
 
 
 circuit_files = pd.read_csv('../data/sc_lc.csv', low_memory=False)
 
 
-
+file_availability = {}
 count = 0
 for file,term in zip(circuit_files['caseid'],circuit_files['term']):
-    path =  '../data/cleaned_Mar_28_python2/' + str(term) + '/maj/'+file+"-maj.p"
+    found = False
+    current_term = int(term)
+    if file in file_availability:
+        continue
+
+    while term-current_term <= 7:
+        path =  '../data/cleaned_Mar_28_python2/' + str(current_term) + '/maj/'+file+"-maj.p"
+        if os.path.exists(path):
+            found = True
+            break
+        current_term = current_term - 1
+
+    if found:
+        file_availability[file] = current_term
+    else:
+        file_availability[file] = "Not found"
+
+
     try:
         with open(path, "rb") as f:
             w = pickle.load(f)
-            pickle.dump(w, open('../data/circuit-scbd-mapped-files/'+file+'.p',"wb"), protocol=2)
+            pickle.dump(w, open('../data/circuit-scbd-mapped-files-complete/'+file+'.p',"wb"), protocol=2)
     except Exception as e:
         count = count + 1
         print e
