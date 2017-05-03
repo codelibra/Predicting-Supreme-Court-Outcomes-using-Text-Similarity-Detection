6040d4d51bf63bd2f7b66431ddf5e49121116de0
akshaytrivedi <trivedi.akshay3@gmail.com>
2017-05-03 15:33:19 -0400

    Adding sc lc generator

---
+++ pythonScripts/sc_lc_generator.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+# We are using scdb docket centered data because it doesn't consolidate cases
+# Below two functions are made available by Micheal Bommirito's paper
+raw_data = get_raw_scdb_data('SCDB_2016_01_justiceCentered_Docket.csv')
+scbd_dataframe = preprocess_raw_data(raw_data, include_direction=True)
+
+
+sc_lc_link_dataframe = pd.read_csv('sc_lc_link.csv', header=0)
+
+sc_lc_link_dataframe['key'] = sc_lc_link_dataframe['term'].map(str) + '-' + sc_lc_link_dataframe['docket']
+scbd_dataframe['key'] = scbd_dataframe['term'].map(str) + '-' + scbd_dataframe['docket']
+
+scbd_no_lc_dataframe = scbd_dataframe[~scbd_dataframe['key'].isin(sc_lc_link_dataframe['key'])]
+scbd_no_lc_dataframe.shape
+scbd_dataframe.shape
+
+scbd_dataframe = scbd_dataframe[scbd_dataframe['key'].isin(sc_lc_link_dataframe['key'])]
+scbd_dataframe.shape
+
+sc_lc_link_cols_to_use = sc_lc_link_dataframe.columns.difference(scbd_dataframe.columns)
+sc_lc_link_cols_to_use = sc_lc_link_cols_to_use | ['key']
+merged_dataframe = scbd_dataframe.merge(sc_lc_link_dataframe[sc_lc_link_cols_to_use], on='key', how='left', suffixes=('', '_y'))
+
+merged_dataframe.to_csv('sc_lc.csv',header=True,index=False)
+# The program ends here
+
+# This is just some testing to show which dataset to use. Need not run this.
+docket_dataframe = pd.read_csv('SCDB_2016_01_caseCentered_Docket.csv', header=0)
+docket_dataframe['docket'].value_counts()
+group_by_dataframe = docket_dataframe.groupby(['usCite'])
+group_by_with_clause_dataframe = group_by_dataframe.filter(lambda x: len(x) == 3)
+
+group_by_with_clause_dataframe.shape
+group_by_with_clause_dataframe[['term','docket','usCite','caseDisposition']]
